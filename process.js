0e2b07415c9ac73f52742ad740402cc5e01b6cab
Subhajit Sahu <wolfram77@gmail.com>
2024-10-12 11:43:25 +0530

    :bug: process output logs

--- process.js
+++ process.js
@@ -5,7 +5,7 @@ const path = require('path');
 const ROMPTH = /^OMP_NUM_THREADS=(\d+)/;
 const RGRAPH = /^Loading graph .*\/(.*?)\.mtx \.\.\./m;
 const RORDER = /^order: (\d+) size: (\d+) (?:\[\w+\] )?\{\}/m;
-const RRESLT = /^\{-(.+?)\/\+(.+?) batchf, (.+?) threads\} -> \{(.+?)ms, (.+?)ms mark, (.+?)ms init, (.+?)ms firstpass, (.+?)ms locmove, (.+?)ms refine, (.+?)ms aggr, (.+?) aff, (.+?) iters, (.+?) passes, (.+?) modularity, (.+?)\/(.+?) disconnected\} (.+)/m;
+const RRESLT = /^\{-(.+?)\/\+(.+?) batchf, (.+?) threads\} -> \{(.+?)ms, (.+?)ms mark, (.+?)ms init, (.+?)ms firstpass, (.+?)ms locmove, (.+?)ms refine, (.+?)ms aggr, (.+?)ms track, (.+?) aff, (.+?) iters, (.+?) passes, (.+?) modularity, (.+?)\/(.+?) disconnected\} (.+)/m;
 
 
 
@@ -60,7 +60,7 @@ function readLogLine(ln, data, state) {
     state.size  = parseFloat(size);
   }
   else if (RRESLT.test(ln)) {
-    var [, batch_deletions_fraction, batch_insertions_fraction, num_threads, time, marking_time, initialization_time, first_pass_time, local_moving_phase_time, refinement_phase_time, aggregation_phase_time, affected_vertices, iterations, passes, modularity, disconnected_communities, total_communities, technique] = RRESLT.exec(ln);
+    var [, batch_deletions_fraction, batch_insertions_fraction, num_threads, time, marking_time, initialization_time, first_pass_time, local_moving_phase_time, refinement_phase_time, aggregation_phase_time, tracking_phase_time, affected_vertices, iterations, passes, modularity, disconnected_communities, total_communities, technique] = RRESLT.exec(ln);
     data.get(state.graph).push(Object.assign({}, state, {
       batch_deletions_fraction:  parseFloat(batch_deletions_fraction),
       batch_insertions_fraction: parseFloat(batch_insertions_fraction),
@@ -72,6 +72,7 @@ function readLogLine(ln, data, state) {
       local_moving_phase_time: parseFloat(local_moving_phase_time),
       refinement_phase_time:   parseFloat(refinement_phase_time),
       aggregation_phase_time:  parseFloat(aggregation_phase_time),
+      tracking_phase_time:     parseFloat(tracking_phase_time),
       affected_vertices:       parseFloat(affected_vertices),
       iterations:  parseFloat(iterations),
       passes:      parseFloat(passes),
