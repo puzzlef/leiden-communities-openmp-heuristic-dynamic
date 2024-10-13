7ffbc08af8c8e0853ce3963be686b2ba6ab8e9f3
Subhajit Sahu <wolfram77@gmail.com>
2024-10-14 01:16:48 +0530

    :bug: with scaling experiment

--- main.sh
+++ main.sh
@@ -19,25 +19,26 @@ if [[ "$DOWNLOAD" != "0" ]]; then
   rm -rf $src
   git clone https://github.com/puzzlef/$src
   cd $src
+  git checkout large-scaling
 fi
 
 # Fixed config
 : "${TYPE:=float}"
 : "${MAX_THREADS:=64}"
-: "${REPEAT_BATCH:=5}"
+: "${REPEAT_BATCH:=1}"
 : "${REPEAT_METHOD:=1}"
 # Parameter sweep for batch (randomly generated)
 : "${BATCH_UNIT:=%}"
 : "${BATCH_LENGTH:=1}"
-: "${BATCH_DELETIONS_BEGIN:=0.00000002}"
-: "${BATCH_DELETIONS_END:=0.02}"
+: "${BATCH_DELETIONS_BEGIN:=0.0002}"
+: "${BATCH_DELETIONS_END:=0.0002}"
 : "${BATCH_DELETIONS_STEP:=*=10}"
-: "${BATCH_INSERTIONS_BEGIN:=0.00000008}"
-: "${BATCH_INSERTIONS_END:=0.08}"
+: "${BATCH_INSERTIONS_BEGIN:=0.0008}"
+: "${BATCH_INSERTIONS_END:=0.0008}"
 : "${BATCH_INSERTIONS_STEP:=*=10}"
 # Parameter sweep for number of threads
 : "${NUM_THREADS_MODE:=all}"
-: "${NUM_THREADS_BEGIN:=64}"
+: "${NUM_THREADS_BEGIN:=1}"
 : "${NUM_THREADS_END:=64}"
 : "${NUM_THREADS_STEP:=*=2}"
 # Define macros (dont forget to add here)
